eed2a0cddcec360c27efd492f332c634728fb9a7
正木啓介 <kettan0130@gmail.com>
2021-09-30 07:29:15 +0900

    画像ファイルアップロード

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,22 @@
 class BoardsController < ApplicationController
     def index
+        @boards = Board.all
+    end
+
+    def show
     end
 
     def new
+        @board = current_user.boards.build
+    end
+
+    def create
+        @board = Board.new(board_params)
+    end
+
+
+    private
+    def board_params
+        params.require(:board).permit(:name, :description) 
     end
 end
