b8bb48cc4ce3378e0e67a2f714c056a73a46e121
正木啓介 <kettan0130@gmail.com>
2021-10-02 09:20:58 +0900

    boardlistのUIを作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,22 +1,26 @@
 class BoardsController < ApplicationController
-    def index
-        @boards = Board.all
-    end
+  def index
+    @boards = Board.all
+  end
 
-    def show
-    end
+  def new
+    @board = Board.new
+  end
 
-    def new
-        @board = current_user.boards.build
+  def create
+    @board = Board.create(board_params)
+    if @board.save
+      redirect_to root_path, notice: '保存できました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
     end
-
-    def create
-        @board = Board.new(board_params)
-    end
+  end
 
 
-    private
-    def board_params
-        params.require(:board).permit(:name, :description) 
-    end
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :description)
+  end
 end
