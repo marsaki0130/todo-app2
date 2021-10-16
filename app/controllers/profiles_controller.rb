d72d91146a7cd1e54300130706d96c299524c87b
正木啓介 <kettan0130@gmail.com>
2021-10-16 10:23:22 +0900

    profileのUI・編集・削除作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,5 +7,21 @@ class ProfilesController < ApplicationController
     end
 
     def edit
+      @profile = current_user.build_profile #has_oneの時はbuild_profileにする(#current_user.boards.buildはhas_manyのとき)
+    end
+
+    def update
+      @profile = current_user.build_profile(profile_params)
+      if @profile.save
+        redirect_to profile_path, notice: 'プロフィールを更新しました'
+      else
+        flash.now[:error] = '更新に失敗しました'
+        render :edit
+      end
+    end
+
+    private
+    def profile_params
+      params.require(:profile).permit(:avatar, :nickname)
     end
 end
